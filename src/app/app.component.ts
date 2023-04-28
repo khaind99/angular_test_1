51e20fed5dd8dea8c5103c9dbc9fa88de9971d26
tuannm6 <tuannm6@tng-holdings.vn>
2023-04-28 15:27:37 +0700

    tuan submit product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-test-54a';
+export class AppComponent implements OnInit {
+  title = 'Khaind';
+  arrProduct = [
+    {
+      first: "Mark",
+      last: "Otto",
+      handle: "@mdo",
+      image: "https://cdn-amz.woka.io/images/I/81Vr0-VQhOL.jpg"
+    },
+    {
+      first: "Jacob",
+      last: "Thornton",
+      handle: "@fat",
+      image: "https://cdn-amz.woka.io/images/I/81Vr0-VQhOL.jpg"
+    },
+    {
+      first: "Larry the Bird",
+      last: "ABC",
+      handle: "@twitter",
+      image: "https://cdn-amz.woka.io/images/I/81Vr0-VQhOL.jpg"
+    },
+  ]
+
+  ngOnInit() {
+    console.log('abc', this.title)
+  }
 }
